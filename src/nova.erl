818b6da5baf8e0e85f49326e579592d9ef867dd6
Niclas Axelsson <niclas@burbas.se>
2019-09-01 22:06:43 +0200

    Fix specs and remove copyright

--- src/nova.erl
+++ src/nova.erl
@@ -1,9 +1,7 @@
 %%% @author Niclas Axelsson <niclas@burbas.se>
-%%% @copyright (C) 2019, Niclas Axelsson
 %%% @doc
 %%% Interface module for nova
 %%% @end
-%%% Created : 19 Feb 2019 by Niclas Axelsson <niclas@burbas.se>
 
 -module(nova).
 
@@ -19,9 +17,9 @@
 %% Returns the name of the main bw-application (The one that started
 %% everything)
 %%
-%% @spec get_main_app() -> {ok, Application :: atom()}
 %% @end
 %%--------------------------------------------------------------------
+-spec get_main_app() -> {ok, Application :: atom()}.
 get_main_app() ->
     nova_router:get_main_app().
 
@@ -29,9 +27,9 @@ get_main_app() ->
 %% @doc
 %% Checks if a nova application is loaded
 %%
-%% @spec application_loaded(Application::atom()) -> true | false
 %% @end
 %%--------------------------------------------------------------------
+-spec application_loaded(Application::atom()) -> boolean().
 application_loaded(Application) ->
     %% First we need to check which application that is the main one
     {ok, MainApp} = application:get_application(nova_sup),
@@ -42,8 +40,8 @@ application_loaded(Application) ->
 %% @doc
 %% Loads a route-file for a certain application
 %%
-%% @spec load_routefile(Application :: atom(), RoutePath :: string()) -> ok
 %% @end
 %%--------------------------------------------------------------------
+-spec spec load_routefile(Application :: atom(), RoutePath :: string()) -> ok.
 load_routefile(Application, RoutePath) ->
     nova_router:process_routefile(Application, RoutePath).
