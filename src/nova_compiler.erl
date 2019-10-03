59f102dc5fa2e017c4de2bf298256e47806f2550
Niclas Axelsson <niclas@burbas.se>
2019-10-03 20:55:33 +0200

    Remake the compiler to one without gen_server

--- src/nova_compiler.erl
+++ src/nova_compiler.erl
@@ -5,176 +5,24 @@
 %%% @end
 -module(nova_compiler).
 
--behaviour(gen_server).
-
 %% API
 -export([
-         start_link/0,
-         compile_all/0,
-         compile_all/1,
-         do_compile/1,
-         get_views/0,
-         recompile_view/1
-        ]).
-
-%% gen_server callbacks
--export([
-         init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3
+         compile_all/0
         ]).
 
 -include_lib("nova/include/nova.hrl").
 
--define(SERVER, ?MODULE).
-
--record(state, {
-          compiled_modules
-         }).
-
 %%%===================================================================
 %%% API
 %%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-
-compile_all(App) ->
-    gen_server:cast(?SERVER, {compile_app_views, App}).
-
 compile_all() ->
     Apps =
         case application:get_env(nova_applications) of
             undefined -> [];
             {ok, AppList} -> AppList
         end,
-    lists:foreach(fun compile_all/1, Apps),
-    ok.
+    lists:foreach(fun compile_files/1, Apps).
 
-get_views() ->
-    gen_server:call(?SERVER, get_views).
-
-recompile_view(Viewname) ->
-    gen_server:call(?SERVER, {recompile, Viewname}).
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
-init([]) ->
-    compile_all(),
-    process_flag(trap_exit, true),
-    {ok, #state{compiled_modules = #{}}}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_call(get_views, _From, State = #state{compiled_modules = CM}) ->
-    {reply, {ok, CM}, State};
-handle_call({recompile, Viewname}, _From, State = #state{compiled_modules = CM}) ->
-    case maps:get(Viewname, CM, undefined) of
-        undefined ->
-            ?WARNING("Tried to recompile a module (~p) that does not exists?", [Viewname]),
-            {reply, {error, view_not_found}, State};
-        File ->
-            do_compile([File]),
-            {reply, ok, State}
-    end;
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_cast({compile_app_views, App}, State = #state{compiled_modules = CM}) ->
-    CM2 =
-        case compile_files(App) of
-            error ->
-                CM;
-            ModMap ->
-                maps:merge(ModMap, CM)
-        end,
-    {noreply, State#state{compiled_modules = CM2}};
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
@@ -187,7 +35,7 @@ compile_files(#{name := App}) ->
         Filepath ->
             {ok, FilesAndDirs} = file:list_dir(Filepath),
             Result = [ do_compile(filename:join([Filepath, FileOrDir])) || FileOrDir <- FilesAndDirs ],
-            maps:from_list(lists:flatten(Result))
+            {ok, maps:from_list(lists:flatten(Result))}
     end.
 
 do_compile(File) ->
