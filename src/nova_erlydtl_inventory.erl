dee797f33e3c8ee031e57ff954446164764df949
Niclas Axelsson <niclas@burbas.se>
2020-02-11 21:52:54 +0100

    New erlydtl-inventory for nova. Adds a new tag 'url' which will prefix the tag if needed.

---
+++ src/nova_erlydtl_inventory.erl
@@ -0,0 +1,26 @@
+-module(nova_erlydtl_inventory).
+-behaviour(erlydtl_library).
+-include_lib("nova/include/nova.hrl").
+
+-export([
+         version/0,
+         inventory/1,
+         url/2
+        ]).
+
+version() -> 1.
+
+inventory(filters) -> [];
+inventory(tags) -> [url].
+
+url(Variables, _Options) ->
+    Url = hd(Variables),
+    App = proplists:get_value(application, Variables),
+    case nova_router:get_app(binary_to_atom(App, utf8)) of
+        {error, _} ->
+            ?WARNING("Could not find application: ~p. Called from erlydtl-tag 'url'.", [App]),
+            <<"">>;
+        {ok, #{prefix := Prefix}} ->
+            PrefixBin = list_to_binary(Prefix),
+            << PrefixBin/binary, Url/binary >>
+    end.
