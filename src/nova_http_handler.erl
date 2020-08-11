bb613f16b185884ee2f234306d082f614c79ed57
Daniel Widgren <daniel.widgren@gmail.com>
2020-08-11 20:02:10 +0200

    fixed elvis spaces and tabs

--- src/nova_http_handler.erl
+++ src/nova_http_handler.erl
@@ -43,7 +43,7 @@
 %% @end
 %%--------------------------------------------------------------------
 -spec init(Req :: cowboy_req:req(), State :: nova_http_state()) ->
-		  {ok, Req0 :: cowboy_req:req(), State0 :: nova_http_state()}.
+                  {ok, Req0 :: cowboy_req:req(), State0 :: nova_http_state()}.
 init(Req, State) ->
     State0 = State#{resp_status => 200, req => Req},
 
