392fd3d5e70a668246c0f648651ffdf7424f53e0
Daniel Widgren <daniel.widgren@gmail.com>
2020-08-13 14:23:41 +0200

    added correlation and cors plugin

---
+++ src/nova_cors_plugin.erl
@@ -0,0 +1,25 @@
+-module(nova_cors_plugin).
+-behaviour(nova_plugin).
+
+
+-export([pre_request/2,
+         post_request/2,
+         plugin_info/0]).
+
+pre_request(#{req := Req} = NovaState, _) ->
+    Req1 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, <<"*">>, Req),
+    Req2 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Headers">>, <<"*">>, Req1),
+    Req3 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Methods">>, <<"*">>, Req2),
+    NewState = maps:put(req, Req3, NovaState),
+    case Req of
+        #{method := <<"OPTIONS">>} ->
+            {stop, NewState};
+        #{method := _} ->
+            {ok, maps:put(req, Req1, NovaState)}
+    end.
+
+post_request(NovaState, _) ->
+    {ok, NovaState}.
+
+plugin_info() ->
+    {<<"nova_cors_plugin">>, <<"0.1.0">>, <<"">>, <<"Add CORS headers to request">>}.
