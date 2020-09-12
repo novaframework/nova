5ecee2decf34fba5aac7735abf769c3467630cfa
Daniel Widgren <daniel.widgren@gmail.com>
2020-09-13 00:04:27 +0200

    Changed cors and correlation plugin to X_http_request instaed of X_request

--- src/nova_cors_plugin.erl
+++ src/nova_cors_plugin.erl
@@ -2,11 +2,11 @@
 -behaviour(nova_plugin).
 
 
--export([pre_request/2,
-         post_request/2,
+-export([pre_http_request/2,
+         post_http_request/2,
          plugin_info/0]).
 
-pre_request(#{req := Req} = NovaState, _) ->
+pre_http_request(#{req := Req} = NovaState, _) ->
     Req1 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, <<"*">>, Req),
     Req2 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Headers">>, <<"*">>, Req1),
     Req3 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Methods">>, <<"*">>, Req2),
@@ -18,7 +18,7 @@ pre_request(#{req := Req} = NovaState, _) ->
             {ok, maps:put(req, Req1, NovaState)}
     end.
 
-post_request(NovaState, _) ->
+post_http_request(NovaState, _) ->
     {ok, NovaState}.
 
 plugin_info() ->
