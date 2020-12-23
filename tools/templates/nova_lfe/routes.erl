8e3cb0f626500f3bca02264597b5e326a0c14206
Daniel Widgren <daniel.widgren@gmail.com>
2020-12-23 09:58:57 +0100

    more lfe templating

--- tools/templates/nova_lfe/routes.erl
+++
@@ -1,9 +0,0 @@
-#{prefix => "",
-  security => false,
-  routes => [
-            {"/", { {{name}}_main_controller, index}, #{methods => [get]}}
-           ],
- statics => [
-             {"/assets/[...]", "assets"}
-            ]
-}.
