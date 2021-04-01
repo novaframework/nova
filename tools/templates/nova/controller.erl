1f54462372cf43ceff757d99aea2212f8da31c5c
Daniel Widgren <daniel.widgren@gmail.com>
2021-04-01 14:07:06 +0200

    removed tools, have been moved rebar3_nova

--- tools/templates/nova/controller.erl
+++
@@ -1,7 +0,0 @@
--module({{name}}_main_controller).
--export([
-         index/1
-        ]).
-
-index(#{req := #{method := <<"GET">>}} = _NovaReq) ->
-    {ok, [{message, "Nova is running!"}]}.
--- tools/templates/nova_lfe/routes.lfe
+++
@@ -1,5 +0,0 @@
-#m(prefix ""
-   security false
-   routes (#("/"
-             #({{name}}_main_controller index)
-             #M(methods (get)))))
