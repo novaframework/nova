ba0072dc5a47ef98d78c06892ad96d37066138d6
Niclas Axelsson <niclas@burbas.se>
2021-10-12 21:37:52 +0200

    Fix case for websocket

--- src/nova_security_handler.erl
+++ src/nova_security_handler.erl
@@ -14,7 +14,13 @@ execute(Req, Env = #{secure := false}) ->
 execute(Req = #{host := Host, path := Path, method := Method}, Env = #{secure := {Module, Function}}) ->
     try Module:Function(Req) of
         {true, AuthData} ->
-            {ok, Req, Env#{controller_data => #{auth_data => AuthData}}};
+            case maps:get(cowboy_handler, Env) of
+                nova_ws_handler ->
+                    Args = maps:get(arguments, Env, #{}),
+                    {ok, Req, Env#{arguments => Args#{controller_data => #{auth_data => AuthData}}}};
+                _ ->
+                    {ok, Req, Env#{controller_data => #{auth_data => AuthData}}}
+            end;
         true ->
             {ok, Req, Env};
         {false, Headers} ->
@@ -28,4 +34,6 @@ execute(Req = #{host := Host, path := Path, method := Method}, Env = #{secure :=
         _ ->
             {ok, Req0, _Env0} = nova_router:render_status_page(Host, 401),
             {stop, Req0}
+    catch _Class:_Reason ->
+            nova_router:render_status_page(Req, 500)
     end.
