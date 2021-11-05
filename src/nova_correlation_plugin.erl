d2715a3228076eada7d3236c71090087b620ef7e
Daniel Widgren <daniel.widgren@gmail.com>
2021-11-05 10:51:41 +0100

    fixed cors and correlation plugin

--- src/nova_correlation_plugin.erl
+++ src/nova_correlation_plugin.erl
@@ -1,18 +1,17 @@
 -module(nova_correlation_plugin).
 -behaviour(nova_plugin).
 
--export([pre_http_request/2,
-         post_http_request/2,
+-export([pre_request/2,
+         post_request/2,
          plugin_info/0]).
 
-pre_http_request(#{req := Req} = NovaState, _) ->
+pre_request(Req, _) ->
     UUID = uuid:uuid_to_string(uuid:get_v4()),
     Req1 = cowboy_req:set_resp_header(<<"x-correlation-id">>, UUID, Req),
-    NewState = maps:put(req, Req1, NovaState),
-    {ok, NewState}.
+    {ok, Req1}.
 
-post_http_request(NovaState, _) ->
-    {ok, NovaState}.
+post_request(Req, _) ->
+    {ok, Req}.
 
 plugin_info() ->
     {<<"nova_cors_plugin">>, <<"0.1.0">>, <<"">>, <<"Add CORS headers to request">>, []}.
