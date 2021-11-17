5cca591f5a587c028122e0b406f9bec9c067234a
Fredrik <fredrik.d.holmqvist@gmail.com>
2021-11-17 09:23:13 +0100

    0.2.0

--- src/nova_cors_plugin.erl
+++ src/nova_cors_plugin.erl
@@ -6,19 +6,20 @@
          post_request/2,
          plugin_info/0]).
 
-pre_request(Req, _) ->
-    Req1 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, <<"*">>, Req),
+pre_request(#{headers := #{<<"origin">> := Origin}, method := <<"OPTIONS">>} = Req, _) ->
+    CorsReq = add_cors_headers(Origin, Req),
+    Req2 = cowboy_req:reply(200, CorsReq),
+    {stop, Req2};
+pre_request(#{headers := #{<<"origin">> := Origin}} = Req, _) -> {ok, add_cors_headers(Origin, Req)};
+pre_request(Req, _) -> {ok, add_cors_headers(<<"null">>, Req)}.
+
+add_cors_headers(Origin, Req) ->
+    Req1 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, Origin, Req),
     Req2 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Headers">>, <<"*">>, Req1),
-    Req3 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Methods">>, <<"*">>, Req2),
-    case Req3 of
-        #{method := <<"OPTIONS">>} ->
-            {ok, Req3};
-        #{method := _} ->
-            {ok, Req3}
-    end.
+    cowboy_req:set_resp_header(<<"Access-Control-Allow-Methods">>, <<"*">>, Req2).
 
 post_request(Req, _) ->
     {ok, Req}.
 
 plugin_info() ->
-    {<<"nova_cors_plugin">>, <<"0.1.0">>, <<"">>, <<"Add CORS headers to request">>, []}.
+    {<<"nova_cors_plugin">>, <<"0.2.0">>, <<"">>, <<"Add CORS headers to request">>, []}.
