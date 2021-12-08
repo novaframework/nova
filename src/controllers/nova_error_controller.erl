688aed60dc5ff8b9d520c6089387b647be43c56e
Niclas Axelsson <niclas@burbas.se>
2021-12-08 11:34:57 +0100

    Correct function-name

--- src/controllers/nova_error_controller.erl
+++ src/controllers/nova_error_controller.erl
@@ -8,7 +8,7 @@ not_found(_Req) ->
     Variables = #{status => "Could not find the page you were looking for",
                   title => "404 Not found",
                   message => "We could not find the page you were looking for"},
-    {ok, Body} = nova_error:render(Variables),
+    {ok, Body} = nova_error_dtl:render(Variables),
     {status, 404, #{}, Body}.
 
 server_error(#{crash_info := #{stacktrace := Stacktrace, class := Class, reason := Reason}}) ->
@@ -17,5 +17,5 @@ server_error(#{crash_info := #{stacktrace := Stacktrace, class := Class, reason 
                   message => "Something internal crashed. Please take a look!",
                   extra_msg => io_lib:format("~p, ~p", [Class, Reason]),
                   stacktrace => Stacktrace}, #{view => nova_error},
-    {ok, Body} = nova_error:render(Variables),
+    {ok, Body} = nova_error_dtl:render(Variables),
     {status, 500, #{}, Body}.
