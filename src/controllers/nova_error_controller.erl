19b796b7c9a9d2c9f03abffa6284992adbe8dd22
Niclas Axelsson <niclas@burbas.se>
2021-12-08 11:10:18 +0100

    Fix that we return the correct statuses

--- src/controllers/nova_error_controller.erl
+++ src/controllers/nova_error_controller.erl
@@ -5,13 +5,17 @@
         ]).
 
 not_found(_Req) ->
-    {ok, #{status => "Could not find the page you were looking for",
-           title => "404 Not found",
-           message => "We could not find the page you were looking for"}, #{view => nova_error}}.
+    Variables = #{status => "Could not find the page you were looking for",
+                  title => "404 Not found",
+                  message => "We could not find the page you were looking for"},
+    {ok, Body} = nova_error:render(Variables),
+    {status, 404, #{}, Body}.
 
 server_error(#{crash_info := #{stacktrace := Stacktrace, class := Class, reason := Reason}}) ->
-    {ok, #{status => "Internal Server Error",
-           title => "500 Internal Server Error",
-           message => "Something internal crashed. Please take a look!",
-           extra_msg => io_lib:format("~p, ~p", [Class, Reason]),
-           stacktrace => Stacktrace}, #{view => nova_error}}.
+    Variables = #{status => "Internal Server Error",
+                  title => "500 Internal Server Error",
+                  message => "Something internal crashed. Please take a look!",
+                  extra_msg => io_lib:format("~p, ~p", [Class, Reason]),
+                  stacktrace => Stacktrace}, #{view => nova_error},
+    {ok, Body} = nova_error:render(Variables),
+    {status, 500, #{}, Body}.
