2d8aff20e39ac61c02f9f3c0eb33bf0b7d297b1d
Niclas Axelsson <niclas@burbas.se>
2021-12-20 14:24:16 +0100

    Use logger instead of nova macros

--- src/plugins/nova_correlation_plugin.erl
+++ src/plugins/nova_correlation_plugin.erl
@@ -7,6 +7,8 @@
 
 pre_request(Req, _) ->
     UUID = uuid:uuid_to_string(uuid:get_v4()),
+    %% Update the loggers metadata with correlation-id
+    logger:update_process_metadata(#{<<"correlation-id">> => UUID}),
     Req1 = cowboy_req:set_resp_header(<<"X-Correlation-ID">>, UUID, Req),
     {ok, Req1}.
 
@@ -15,9 +17,9 @@ post_request(Req, _) ->
 
 plugin_info() ->
     {
-     <<"nova_correlation_plugin">>, 
-     <<"0.1.0">>, 
+     <<"nova_correlation_plugin">>,
+     <<"0.1.0">>,
      <<"Nova team <info@novaframework.org">>,
-     <<"Add X-Correlation-ID headers to response">>, 
+     <<"Add X-Correlation-ID headers to response">>,
      []
     }.
