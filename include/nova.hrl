d42859ea447d237cacb672b96718c852a2f2dcd3
Niclas Axelsson <niclas@burbas.se>
2021-12-20 13:29:43 +0100

    Use logger and formatters instead of hard-coding the logging-format

--- include/nova.hrl
+++ include/nova.hrl
@@ -9,21 +9,16 @@
 -define(TERM_BOLD, "\033[1m").
 -define(TERM_RESET, "\033[m").
 
--define(MOD_INFO, ?TERM_BOLD ++ "[" ++ erlang:atom_to_list(?MODULE) ++ "] " ++ ?TERM_RESET).
+-define(DEBUG(M), ?LOG(debug, M)).
+-define(DEBUG(M, Meta), ?LOG(debug, M, Meta)).
+-define(INFO(M), ?LOG(info, M)).
+-define(INFO(M, Meta), ?LOG(info, M, Meta)).
+-define(NOTICE(M), ?LOG(notice, M)).
+-define(NOTICE(M, Meta), ?LOG(notice, M, Meta)).
+-define(WARNING(M), ?LOG(warning, M)).
+-define(WARNING(M, Meta), ?LOG(warning, M, Meta)).
+-define(ERROR(M), ?LOG(error, M)).
+-define(ERROR(M, Meta), ?LOG(error, M, Meta)).
 
--define(DEBUG(M), ?LOG(debug, ?MOD_INFO ++ ?TERM_BLUE ++ M ++ ?TERM_RESET)).
--define(DEBUG(M, Meta), ?LOG(debug, ?MOD_INFO ++ ?TERM_BLUE ++ M ++ ?TERM_RESET, Meta)).
--define(INFO(M), ?LOG(info, ?MOD_INFO ++ ?TERM_GREEN ++ M ++ ?TERM_RESET)).
--define(INFO(M,Meta), ?LOG(info, ?MOD_INFO ++ ?TERM_GREEN ++ M ++ ?TERM_RESET, Meta)).
--define(NOTICE(M), ?LOG(notice, ?MOD_INFO ++ ?TERM_GREEN ++ M ++ ?TERM_RESET)).
--define(NOTICE(M,Meta), ?LOG(notice, ?MOD_INFO ++ ?TERM_GREEN ++ M ++ ?TERM_RESET, Meta)).
--define(WARNING(M), ?LOG(warning, ?MOD_INFO ++ ?TERM_YELLOW ++ M ++ ?TERM_RESET)).
--define(WARNING(M,Meta), ?LOG(warning, ?MOD_INFO ++ ?TERM_YELLOW ++ M ++ ?TERM_RESET, Meta)).
--define(ERROR(M), ?LOG(error, ?MOD_INFO ++ ?TERM_RED ++ M ++ ?TERM_RESET)).
--define(ERROR(M,Meta), ?LOG(error, ?MOD_INFO ++ ?TERM_RED ++ M ++ ?TERM_RESET, Meta)).
-
-%% Meta levels
--define(DEPRECATION(M), ?LOG(warning, ?TERM_YELLOW ++ ?TERM_BOLD ++ "DEPRECATION WARNING! " ++ ?TERM_RESET ++ ?TERM_YELLOW ++ M ++ ?TERM_RESET)).
--define(DEPRECATED(M), ?LOG(error, ?TERM_RED ++ ?TERM_BOLD ++ "DEPRECATION ERROR!!! " ++ ?TERM_RESET ++ ?TERM_RED ++ M ++ ?TERM_RESET)).
 
 -endif.
