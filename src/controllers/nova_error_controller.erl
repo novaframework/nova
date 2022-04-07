652a6c6c421e0bc58a4cee5be499e10395e46da9
Niclas Axelsson <niclas@burbas.se>
2022-04-07 16:41:49 +0200

    Display a special error if the controller/function could not be found

--- src/controllers/nova_error_controller.erl
+++ src/controllers/nova_error_controller.erl
@@ -21,11 +21,31 @@ not_found(Req) ->
             {status, 404, #{<<"content-type">> => <<"text/html">>}, Body}
     end.
 
+server_error(#{crash_info := #{status_code := StatusCode} = CrashInfo} = Req) ->
+    Variables = #{status => maps:get(status, CrashInfo, undefined),
+                  title => maps:get(title, CrashInfo, undefined),
+                  message => maps:get(message, CrashInfo, undefined),
+                  extra_msg => maps:get(extra_msg, CrashInfo, undefined),
+                  stacktrace => maps:get(stacktrace, CrashInfo, undefined)},
+    case application:get_env(nova, render_error_pages, true) of
+        true ->
+            case cowboy_req:header(<<"accept">>, Req) of
+                <<"application/json">> ->
+                    {ok, JsonLib} = nova:get_env(json_lib, thoas),
+                    Json = erlang:apply(JsonLib, encode, [Variables]),
+                    {status, StatusCode, #{<<"content-type">> => <<"application/json">>}, Json};
+                _ ->
+                    {ok, Body} = nova_error_dtl:render(Variables),
+                    {status, StatusCode, #{<<"content-type">> => <<"text/html">>}, Body}
+            end;
+        _ ->
+            {status, StatusCode}
+    end;
 server_error(#{crash_info := #{stacktrace := Stacktrace, class := Class, reason := Reason}} = Req) ->
     Variables = #{status => "Internal Server Error",
                   title => "500 Internal Server Error",
                   message => "Something internal crashed. Please take a look!",
-                  extra_msg => io_lib:format("~p, ~p", [Class, Reason]),
+                  extra_msg => io_lib:format("Class: ~p<br /> Reason: ~p", [Class, Reason]),
                   stacktrace => Stacktrace},
 
     case nova:get_environment() of
