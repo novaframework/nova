45c1f29362a22c0d54b9cfbaa974d5bdf9219f5d
Niclas Axelsson <niclas@burbas.se>
2022-10-24 13:20:46 +0200

    Fix stacktrace call

--- src/nova_handler.erl
+++ src/nova_handler.erl
@@ -43,19 +43,19 @@ execute(Req, Env = #{module := Module, function := Function}) ->
         RetObj ->
             call_handler(Module, Function, Req, RetObj, Env, false)
     catch
-        ?STACKTRACE(Class, Reason, Stacktrace)
-        ?LOG_ERROR(#{msg => "Controller crashed",
-                     class => Class,
-                     reason => Reason,
-                     stacktrace => Stacktrace}),
-        terminate(Reason, Req, Module),
-        %% Build the payload object
-        Payload = #{status_code => 500,
-                    stacktrace => Stacktrace,
-                    class => Class,
-                    reason => Reason},
-        render_response(Req#{crash_info => Payload}, Env, 500)
-        end.
+        Class:Reason:Stacktrace ->
+            ?LOG_ERROR(#{msg => "Controller crashed",
+                         class => Class,
+                         reason => Reason,
+                         stacktrace => Stacktrace}),
+            terminate(Reason, Req, Module),
+            %% Build the payload object
+            Payload = #{status_code => 500,
+                        stacktrace => Stacktrace,
+                        class => Class,
+                        reason => Reason},
+            render_response(Req#{crash_info => Payload}, Env, 500)
+    end.
 
 -spec terminate(any(), Req | undefined, module()) -> ok when Req::cowboy_req:req().
 terminate(Reason, Req, Module) ->
