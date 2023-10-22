cea4f09db766c7654f7ebc51b37ddd0db300b7df
Daniel Widgren <daniel.widgren@gmail.com>
2023-10-22 15:25:18 +0200

    solved dialyzer

--- src/plugins/nova_request_plugin.erl
+++ src/plugins/nova_request_plugin.erl
@@ -65,7 +65,7 @@ plugin_info() ->
 modulate_state(Req, []) ->
     {ok, Req};
 
-modulate_state( Req = #{method := Method}, [decode_json_body|Tail]) when Method =:= <<"GET">>; Method =:= <<"DELETE">> ->
+modulate_state( Req = #{method := Method}, [{decode_json_body, true}|Tail]) when Method =:= <<"GET">>; Method =:= <<"DELETE">> ->
     modulate_state(Req, Tail);
 modulate_state(Req = #{headers := #{<<"content-type">> := <<"application/json", _/binary>>}, body := Body}, [{decode_json_body, true}|Tl]) ->
     %% Decode the data
