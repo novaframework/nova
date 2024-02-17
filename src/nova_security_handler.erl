4bf48004c065887c8d35f7b4d52e6c6bbcc04911
Niclas Axelsson <niclas@burbas.se>
2024-02-17 20:49:52 +0100

    Enhance plugins and security modules (#264)

--- src/nova_security_handler.erl
+++ src/nova_security_handler.erl
@@ -12,30 +12,8 @@ execute(Req, Env = #{secure := false}) ->
 execute(Req = #{host := Host}, Env = #{secure := {Module, Function}}) ->
     UseStacktrace = persistent_term:get(nova_use_stacktrace, false),
     try Module:Function(Req) of
-        {true, AuthData} ->
-            case maps:get(cowboy_handler, Env, undefined) of
-                nova_ws_handler ->
-                    Args = maps:get(arguments, Env, #{}),
-                    {ok, Req, Env#{arguments => Args#{controller_data => #{auth_data => AuthData}}}};
-                _ ->
-                    {ok, Req#{auth_data => AuthData}, Env}
-            end;
-        true ->
-            {ok, Req, Env};
-        {false, Headers} ->
-            {ok, Req0, _Env0} = nova_router:render_status_page(Host, 401, #{}, Req, Env),
-            ExistingHeaders = cowboy_req:headers(Req),
-            FinalHeaders = maps:merge(ExistingHeaders, Headers),
-            Req1 = cowboy_req:set_resp_headers(FinalHeaders, Req0),
-            Req2 = cowboy_req:reply(401, Req1),
-            {stop, Req2};
-        {redirect, Route} ->
-            Req0 = cowboy_req:set_resp_headers(#{<<"Location">> => list_to_binary(Route)}, Req),
-            {stop, Req0};
-        _ ->
-            {ok, Req0, _Env0} = nova_router:render_status_page(Host, 401, #{}, Req, Env),
-            Req1 = cowboy_req:reply(401, Req0),
-            {stop, Req1}
+        Result ->
+            handle_response(Result, Req, Env)
     catch
         Class:Reason:Stacktrace when UseStacktrace == true ->
             ?LOG_ERROR(#{msg => <<"Security handler crashed">>,
@@ -60,3 +38,39 @@ execute(Req = #{host := Host}, Env = #{secure := {Module, Function}}) ->
             Req1 = cowboy_req:reply(500, Req0),
             {stop, Req1}
     end.
+
+
+
+handle_response({true, AuthData}, Req, Env) ->
+    case maps:get(cowboy_handler, Env, undefined) of
+        nova_ws_handler ->
+            Args = maps:get(arguments, Env, #{}),
+            {ok, Req, Env#{arguments => Args#{controller_data => #{auth_data => AuthData}}}};
+        _ ->
+            {ok, Req#{auth_data => AuthData}, Env}
+    end;
+handle_response(true, Req, Env) ->
+    {ok, Req, Env};
+handle_response({false, Headers}, Req, Env) ->
+    handle_response({false, 401, Headers, false}, Req, Env);
+handle_response({false, StatusCode, Headers}, Req, Env) ->
+    handle_response({false, StatusCode, Headers, false}, Req, Env);
+handle_response({false, StatusCode, Headers, Body}, Req = #{host := Host}, Env) ->
+    {ok, Req1, _Env1} =
+        case Body of
+            false ->
+                {ok, _Req0, _Env0} = nova_router:render_status_page(Host, StatusCode, #{}, Req, Env);
+            _ ->
+                Req0 = cowboy_req:set_resp_body(Body, Req),
+                {ok, Req0, Env}
+        end,
+    Req2 = cowboy_req:set_resp_headers(Headers, Req1),
+    Req3 = cowboy_req:reply(StatusCode, Req2),
+    {stop, Req3};
+handle_response({redirect, Route}, Req, _Env) ->
+    Req0 = cowboy_req:set_resp_headers(#{<<"Location">> => list_to_binary(Route)}, Req),
+    {stop, Req0};
+handle_response(_, Req = #{host := Host}, Env) ->
+    {ok, Req0, _Env0} = nova_router:render_status_page(Host, 401, #{}, Req, Env),
+    Req1 = cowboy_req:reply(401, Req0),
+    {stop, Req1}.
