4481491009bddcef8a6063be5b548f4715a20d0c
Niclas Axelsson <niclas@burbas.se>
2024-02-17 19:46:47 +0100

    Include Env when calling plugins with arity 2 and check if we are replying

--- src/nova_plugin_handler.erl
+++ src/nova_plugin_handler.erl
@@ -24,18 +24,44 @@ run_plugins([], Callback, Req, Env) ->
     {ok, Req, Env#{plugin_state => Callback}};
 run_plugins([{Module, Options}|Tl], Callback, Req, Env) ->
     ExtraState = maps:get(extra_state, Env, #{}),
-    try Module:Callback(Req#{extra_state => ExtraState}, Options) of
-        {ok, Req0} ->
-            run_plugins(Tl, Callback, Req0, Env);
-        {break, Req0} ->
-            {ok, Req0};
-        {stop, Req0} ->
-            {stop, Req0}
+    Req0 = Req#{extra_state => ExtraState},
+    Args = case proplists:get_value(Callback, Module:module_info(exports)) of
+               1 -> [Req0, Options];
+               2 -> [Req0, Env, Options];
+               _ -> {throw, bad_callback}
+           end,
+    try erlang:apply(Module, Callback, Args) of
+        {ok, Req1} ->
+            run_plugins(Tl, Callback, Req1, Env);
+        {ok, Reply, Req1} ->
+            Req2 = handle_reply(Reply, Req1),
+            run_plugins(Tl, Callback, Req2, Env);
+        {break, Req1} ->
+            {ok, Req1};
+        {break, Reply, Req1} ->
+            Req2 = handle_reply(Reply, Req1),
+            {ok, Req2};
+        {stop, Req1} ->
+            {stop, Req1};
+        {stop, Reply, Req1} ->
+            Req2 = handle_reply(Reply, Req1),
+            {stop, Req2}
     catch
         Class:Reason:Stacktrace ->
             ?LOG_ERROR(#{msg => <<"Plugin crashed">>, class => Class, reason => Reason, stacktrace => Stacktrace}),
-            Req0 = Req#{crash_info => #{class => Class,
-                                        reason => Reason,
-                                        stacktrace => Stacktrace}},
-            nova_router:render_status_page('_', 500, #{}, Req0, Env)
+            Req1 = Req0#{crash_info => #{class => Class,
+                                         reason => Reason,
+                                         stacktrace => Stacktrace}},
+            nova_router:render_status_page('_', 500, #{}, Req1, Env)
     end.
+
+handle_reply({reply, Body}, Req) ->
+    handle_reply({reply, 200, #{}, Body}, Req);
+handle_reply({reply, Status, Body}, Req) ->
+    handle_reply({reply, Status, #{}, Body}, Req);
+handle_reply({reply, Status, Headers, Body}, Req) ->
+    Req0 = cowboy_req:set_resp_headers(Headers, Req),
+    Req1 = cowboy_req:set_resp_body(Body, Req0),
+    Req1#{resp_status_code => Status};
+handle_reply(_, Req) ->
+    Req.
