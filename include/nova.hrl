b1dd57f8aee0d6123c4d763094c2504589d4fa07
Niclas Axelsson <niclas@burbas.se>
2025-06-26 22:54:30 +0200

    Also include file information in the deprecation warnings if such is applicable

--- include/nova.hrl
+++ include/nova.hrl
@@ -1,1 +1,5 @@
--define(LOG_DEPRECATED(SinceVersion, Msg), logger:warning("Deprecation warning. Since version: ~s, Message: ~s~nRead more about deprecations on https://github.com/novaframework/nova/blob/master/guides/deprecations.md", [SinceVersion, Msg])).
+-define(LOG_DEPRECATED(SinceVersion, Msg, File), case File of
+                                                     undefined -> logger:warning("Deprecation warning. Since version: ~s, Message: ~s~nRead more about deprecations on https://github.com/novaframework/nova/blob/master/guides/deprecations.md", [SinceVersion, Msg]);
+                                                     _ ->
+                                                         logger:warning("Deprecation warning. Since version: ~s, Message: ~s~nRead more about deprecations on https://github.com/novaframework/nova/blob/master/guides/deprecations.md\nFound in file: ~s", [SinceVersion, Msg, File])
+                                                 end).
